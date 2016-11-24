6e6c3c94fa1836151e0abd077e44e66dbd8306e4
System Administrator <root@Forrests-Air.home>
2016-11-23 23:27:17 -0500

    lots of work

--- js/forrest.js
+++ js/forrest.js
@@ -12,6 +12,14 @@
                 $("a[href='https://www.instagram.com/riseandpineco/']").attr('href','intent://instagram.com/_u/riseandpineco/#Intent;package=com.instagram.android;scheme=https;end');
             }
     /*End Social Deeplinks*/
+
+    /*Begin Squares vs Tiles
+    if (document.documentElement.clientWidth < 768) {
+        $("#portfolioModal1-image").attr('src','../img/rp-pineyspread-banner-1x.png');
+        $("#portfolioModal2-image").attr('src','../img/rp-citycup-banner-1x.png');
+        $("#portfolioModal3-image").attr('src','../img/rp-mugshot-banner-1x.png');
+    }
+    /*End Squares vs Tiles*/
 })(jQuery); // End of use strict
 
 
