5eaefb183c908ae2eff40460ea1920eae0198ea3
Forrest <forrest.ching@gmail.com>
2017-02-26 14:36:11 -0500

    weekend hackathon

---
+++ js/cms.js
@@ -0,0 +1,16 @@
+//CMS.js
+//The most manual CMS you've ever seen
+
+var cms = {
+	'nav' : {
+		'text' : ['@RISEANDPINECO','Our Vision','Our Story'],
+		'assets' : ['','','']
+	},
+	'hero' : {
+		'image' : '',
+		'text' : {
+			'h1' : '',
+			'h2' : ''
+		}
+	}
+};
